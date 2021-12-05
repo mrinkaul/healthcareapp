518e3d4a505912757592bbc9829c25d21dd48d34
mrinkaul <mrin.kaul@gmail.com>
2021-12-04 22:15:46 -0500

    Added onClickButtonDiagnose

--- app/src/main/java/com/rwc/symptomtracker/frontendjava/SymptomsActivity.java
+++ app/src/main/java/com/rwc/symptomtracker/frontendjava/SymptomsActivity.java
@@ -4,6 +4,8 @@ import static com.rwc.symptomtracker.Constants.GLOBAL_TAG;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,10 +14,17 @@ import com.rwc.symptomtracker.R;
 public class SymptomsActivity extends AppCompatActivity {
     private static final String TAG = GLOBAL_TAG + "SymptomsActivity";
 
+    CheckBox checkboxFever;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_symptoms);
         Log.d(TAG, "Started......");
+    }
+
+    public void onClickButtonDiagnose(View view) {
+        Log.d(TAG, "Diagnose button clicked");
+        checkboxFever = (CheckBox) findViewById(R.id.checkboxFever);
     }
 }
