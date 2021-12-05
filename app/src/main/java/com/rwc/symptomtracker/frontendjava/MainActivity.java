edc9ea33f590fc17889e6262e81069a02286856c
mrinkaul <mrin.kaul@gmail.com>
2021-12-04 21:12:07 -0500

    Added Constants and onClickButtonCheckSymptoms which just logcats - no action just yet

--- app/src/main/java/com/rwc/symptomtracker/frontendjava/MainActivity.java
+++ app/src/main/java/com/rwc/symptomtracker/frontendjava/MainActivity.java
@@ -1,20 +1,35 @@
 package com.rwc.symptomtracker;
 
+import static com.rwc.symptomtracker.Constants.GLOBAL_TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = GLOBAL_TAG + "MainActivity";
+
+    Button buttonCheckSymptoms;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Started......");
+        acquireHandles();
     }
 
-    public void onClick(View view) {
-
+    private void acquireHandles() {
+        // Get handles to various views
+        buttonCheckSymptoms = (Button) findViewById(R.id.buttonCheckSymptoms);
     }
+
+    public void onClickButtonCheckSymptoms(View view) {
+        Log.d(TAG, "Button Check Symptoms");
+    }
+
 }
